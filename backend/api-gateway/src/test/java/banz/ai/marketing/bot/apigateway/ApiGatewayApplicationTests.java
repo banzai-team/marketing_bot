f536d71e73fb5956b37be79811502bf17ec112dc
wuz1mu <kingaal93@gmail.com>
2023-11-04 18:22:50 +0700

    initial gateway

---
+++ backend/api-gateway/src/test/java/banz/ai/marketing/bot/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package banz.ai.marketing.bot.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
