0cc38bfb3a04221d69c97c113a865d9f9eaef242
wuz1mu <kingaal93@gmail.com>
2023-11-04 19:12:19 +0700

    initial model-behavior

---
+++ backend/model-behavior/src/main/java/banz/ai/marketing/bot/modelbehavior/ModelBehaviorApplication.java
@@ -0,0 +1,13 @@
+package banz.ai.marketing.bot.modelbehavior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelBehaviorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelBehaviorApplication.class, args);
+	}
+
+}
---
+++ backend/model-behavior/src/test/java/banz/ai/marketing/bot/modelbehavior/ModelBehaviorApplicationTests.java
@@ -0,0 +1,13 @@
+package banz.ai.marketing.bot.modelbehavior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelBehaviorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
