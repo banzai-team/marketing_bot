5280d3d115556f9aa025b963d403d2638bd87249
wuz1mu <kingaal93@gmail.com>
2023-11-04 19:10:45 +0700

    initial model-interceptor

---
+++ backend/model-interceptor/src/test/java/banz/ai/marketing/bot/modelinterceptor/ModelInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package banz.ai.marketing.bot.modelinterceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
