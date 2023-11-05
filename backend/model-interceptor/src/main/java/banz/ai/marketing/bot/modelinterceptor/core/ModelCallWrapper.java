7418d93dd8fdc3ba62d919f4730d4cb4177d0618
wuz1mu <kingaal93@gmail.com>
2023-11-05 19:10:12 +0700

    fix dto usage

--- backend/model-interceptor/src/main/java/banz/ai/marketing/bot/modelinterceptor/core/ModelCallWrapper.java
+++ backend/model-interceptor/src/main/java/banz/ai/marketing/bot/modelinterceptor/core/ModelCallWrapper.java
@@ -1,8 +1,8 @@
 package banz.ai.marketing.bot.modelinterceptor.core;
 
+import banz.ai.marketing.bot.commons.ModelBehaviorDTO;
 import banz.ai.marketing.bot.commons.ModelRequest;
 import banz.ai.marketing.bot.commons.ModelResponse;
-import banz.ai.marketing.bot.modelinterceptor.dto.MqMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
@@ -20,10 +20,11 @@ public class ModelCallWrapper {
         var resp = modelClient.makeModelRequest(modelRequest);
         var modelResponse = resp.getBody();
 
-        var mqMessage = new MqMessage();
-        mqMessage.setMadeAt(new Date());
-        mqMessage.setModelRequest(modelRequest);
-        mqMessage.setModelResponse(modelResponse);
+        var mqMessage = ModelBehaviorDTO.builder()
+                .capturedAt(new Date())
+                .modelRequest(modelRequest)
+                .modelResponse(modelResponse)
+                .build();
         rabbitTemplate.convertAndSend(mqMessage);
 
         return modelResponse;
