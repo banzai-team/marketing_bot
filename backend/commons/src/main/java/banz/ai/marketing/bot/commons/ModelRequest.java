d2fd99c1cdc20aa91a4f3060ba9128540f4462db
wuz1mu <kingaal93@gmail.com>
2023-11-05 18:17:01 +0700

    squash fix

--- backend/commons/src/main/java/banz/ai/marketing/bot/commons/ModelRequest.java
+++ backend/commons/src/main/java/banz/ai/marketing/bot/commons/ModelRequest.java
@@ -1,12 +1,16 @@
 package banz.ai.marketing.bot.commons;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ModelRequest {
 
     private long dialogId;
--- backend/commons/src/main/java/banz/ai/marketing/bot/commons/UserFeedbackDTO.java
+++ backend/commons/src/main/java/banz/ai/marketing/bot/commons/UserFeedbackDTO.java
@@ -1,13 +1,18 @@
 package banz.ai.marketing.bot.commons;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserFeedbackDTO {
 
     long userId;
     long modelResponseId;
     boolean isCorrect;
+
 }
