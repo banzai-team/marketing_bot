653e911e2f8a5dba027a7bffa1d0c114fee220f9
wuz1mu <kingaal93@gmail.com>
2023-11-07 09:45:30 +0700

    initial user service

---
+++ backend/user-service/src/test/java/banz/ai/marketing/bot/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package banz.ai.marketing.bot.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
