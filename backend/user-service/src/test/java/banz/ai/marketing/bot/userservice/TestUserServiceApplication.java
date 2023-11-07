ed1bf7997c8b1f72b659cf36561c69b868891f64
Aleksey <deshevtsov@isands.ru>
2023-11-07 10:57:29 +0700

    replace gradle with maven

--- backend/user-service/src/test/java/banz/ai/marketing/bot/userservice/TestUserServiceApplication.java
+++ backend/user-service/src/test/java/banz/ai/marketing/bot/userservice/TestUserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -14,6 +15,12 @@ public class TestUserServiceApplication {
 	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
 		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	@Bean
+	@ServiceConnection
+	RabbitMQContainer rabbitContainer() {
+		return new RabbitMQContainer(DockerImageName.parse("rabbitmq:latest"));
 	}
 
 	public static void main(String[] args) {
