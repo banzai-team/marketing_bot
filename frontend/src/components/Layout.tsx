1200b46efd8124c6607cbf2fb501088bb8967868
Julia Astapenko <julia.chvyrova@gmail.com>
2023-11-07 23:29:41 +0300

    Fix components. Fix table

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -16,14 +16,14 @@ const Layout: React.FC = () => {
         
         <div className="h-screen w-screen flex items-center overflow-hidden">
             <div className="flex flex-col justify-center items-center p-6 min-w-100 h-screen relative basis-full sm:basis-5/12 sm:items-end">
-                <img src="logo.svg" className="pt-6 pl-8 absolute top-0 left-0" />
+                <img src="logo.svg" className="pt-6 ml-6 absolute top-0 left-0" width={150} />
                 <div className="text-center py-20 px-10 sm: w-90">
                     <Outlet />
                 </div>
                 <div className="w-96 h-36 bg-primary rounded-full absolute -rotate-45 -bottom-10 -left-36" />
                 <div className="w-96 h-32 bg-primary-content rounded-full absolute -rotate-45 -bottom-10 -left-32" />
             </div>
-            <div className="sm:flex flex-col justify-center items-center h-screen basis-7/12 bg-gradient-to-r from-white from-45% to-primary-content to-45% hidden">
+            <div className="flex-1 sm:flex flex-col justify-center items-center h-screen basis-7/12 bg-gradient-to-r from-base-100 from-45% to-primary-content to-45%">
                 <img src={loginImage} alt="Login image" className="m-auto pb-28" />
             </div>
         </div>
--- frontend/src/screens/Index.tsx
+++ frontend/src/screens/Index.tsx
@@ -1,96 +1,82 @@
-import { Dialog } from '@headlessui/react';
-import { useRef, useState } from 'react';
-import { useAuthState } from '~/components/contexts/UserContext';
+import React from 'react';
 import { Head } from '~/components/shared/Head';
+import Table from "~/components/Table";
+import {createColumnHelper, Row} from "@tanstack/react-table";
 
-function Index() {
-  // const { state } = useAuthState();
-  // const [isOpen, setIsOpen] = useState(true);
-  // const completeButtonRef = useRef(null);
+import {ChevronDownIcon} from '@heroicons/react/24/solid'
+import ExpandedButton from "~/components/ExpandedButton";
+
+
+const Index: React.FC = () => {
+    const data = [
+        {
+            firstName: "Test 1",
+            lastName: "Test 11",
+            age: "111",
+            subRows: [{
+                message: "Expanded data"
+            }]
+        },
+        {
+            firstName: "Test 2",
+            lastName: "Test 22",
+            age: "222",
+            subRows: [{
+                message: "Expanded data"
+            }]
+        },
+        {
+            firstName: "Test 3",
+            lastName: "Test 33",
+            age: "333",
+            subRows: [{
+                message: "Expanded data"
+            }]
+        }
+    ];
+
+    // TODO: add type
+    const columnHelper = createColumnHelper<any>();
+
+    const columns = [
+        columnHelper.accessor('firstName', {
+            cell: ({row, getValue}) => (
+                <div>
+                    <>
+                        <ExpandedButton onClick={row.getToggleExpandedHandler()} isExpanded={row.getIsExpanded()}/>
+                        {getValue()}
+                    </>
+                </div>
+            ),
+        }),
+        columnHelper.accessor(row => row.lastName, {
+            id: 'lastName',
+            cell: info => <i>{info.getValue()}</i>,
+            header: () => <span>Last Name</span>,
+        }),
+        columnHelper.accessor('age', {
+            header: () => 'Age',
+            cell: info => info.renderValue(),
+        })
+    ]
+
+    // TODO: add type
+    const renderSubComponent = ({row}: { row: Row<any> }) => {
+        return (
+            <pre style={{fontSize: '10px'}}>
+      <code>{JSON.stringify(row.original, null, 2)}</code>
+    </pre>
+        )
+    }
 
   return (
     <>
       <Head title="Main page" />
       <div className="page-title">Home Page</div>
-      {/*<div className="hero min-h-screen">*/}
-      {/*  <div className="text-center hero-content">*/}
-      {/*    <div>*/}
-      {/*      <h1 className="text-3xl font-bold">*/}
-      {/*        <a className="link link-primary" target="_blank" href="https://vitejs.dev/" rel="noreferrer">*/}
-      {/*          Vite*/}
-      {/*        </a>{' '}*/}
-      {/*        +{' '}*/}
-      {/*        <a className="link link-primary" target="_blank" href="https://reactjs.org/" rel="noreferrer">*/}
-      {/*          React*/}
-      {/*        </a>{' '}*/}
-      {/*        +{' '}*/}
-      {/*        <a className="link link-primary" target="_blank" href="https://www.typescriptlang.org/" rel="noreferrer">*/}
-      {/*          TypeScript*/}
-      {/*        </a>{' '}*/}
-      {/*        +{' '}*/}
-      {/*        <a className="link link-primary" target="_blank" href="https://tailwindcss.com/" rel="noreferrer">*/}
-      {/*          TailwindCSS*/}
-      {/*        </a>{' '}*/}
-      {/*        Starter*/}
-      {/*      </h1>*/}
-      {/*      <p className="mt-4 text-lg">*/}
-      {/*        For fast <b>prototyping</b>. Already set up{' '}*/}
-      {/*        <a*/}
-      {/*          className="link link-primary"*/}
-      {/*          target="_blank"*/}
-      {/*          href="https://github.com/firebase/firebase-js-sdk"*/}
-      {/*          rel="noreferrer"*/}
-      {/*        >*/}
-      {/*          Firebase(v9)*/}
-      {/*        </a>*/}
-      {/*        ,{' '}*/}
-      {/*        <a className="link link-primary" target="_blank" href="https://daisyui.com/" rel="noreferrer">*/}
-      {/*          daisyUI*/}
-      {/*        </a>*/}
-      {/*        ,{' '}*/}
-      {/*        <a className="link link-primary" target="_blank" href="https://github.com/eslint/eslint" rel="noreferrer">*/}
-      {/*          ESLint*/}
-      {/*        </a>*/}
-      {/*        ,{' '}*/}
-      {/*        <a*/}
-      {/*          className="link link-primary"*/}
-      {/*          target="_blank"*/}
-      {/*          href="https://github.com/prettier/prettier"*/}
-      {/*          rel="noreferrer"*/}
-      {/*        >*/}
-      {/*          Prettier*/}
-      {/*        </a>*/}
-      {/*        .*/}
-      {/*      </p>*/}
-      {/*      /!*<div className="mt-4 grid gap-2">*!/*/}
-      {/*      /!*  {state.state === 'UNKNOWN' ? null : state.state === 'SIGNED_OUT' ? <SignInButton /> : <SignOutButton />}*!/*/}
-      {/*      /!*  <button onClick={() => setIsOpen(true)}>Display Dialog</button>*!/*/}
-      {/*      /!*</div>*!/*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-      {/*<Dialog*/}
-      {/*  className="flex fixed inset-0 z-10 overflow-y-auto"*/}
-      {/*  initialFocus={completeButtonRef}*/}
-      {/*  open={isOpen}*/}
-      {/*  onClose={() => setIsOpen(false)}*/}
-      {/*>*/}
-      {/*  <div className="flex items-center justify-center min-h-screen w-screen">*/}
-      {/*    <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />*/}
-      {/*    <div className="relative bg-white rounded max-w-120 p-8 mx-auto">*/}
-      {/*      <Dialog.Title>Dialog Title</Dialog.Title>*/}
-      {/*      <Dialog.Description>Dialog description</Dialog.Description>*/}
-      {/*      <button*/}
-      {/*        ref={completeButtonRef}*/}
-      {/*        type="button"*/}
-      {/*        className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"*/}
-      {/*        onClick={() => setIsOpen(false)}*/}
-      {/*      >*/}
-      {/*        Got it, thanks!*/}
-      {/*      </button>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</Dialog>*/}
+        <div className="card bg-base-100 shadow-xl overflow-hidden mt-6">
+            <Table data={data} columns={columns} renderSubComponent={renderSubComponent} />
+        </div>
+
     </>
   );
 }
