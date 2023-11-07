82c696d7fb26036cd06596b8ffe54219a07b314a
Julia Astapenko <julia.chvyrova@gmail.com>
2023-11-07 16:16:42 +0300

    Add table component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/solid'
+
+type HeaderProps = {
+    idSidebar: string
+}
+
+const Header: React.FC<HeaderProps> = ({ idSidebar }) => {
+    return (
+        <div className="navbar bg-base-100  lg:hidden md:hidden">
+            <div className="flex-none">
+                <label htmlFor={idSidebar} className=" btn btn-ghost swap btn-square swap-rotate drawer-button">
+                    <input type="checkbox" />
+                    <Bars3Icon className="text-base-content swap-off h-6 w-6" />
+                    <XMarkIcon className="text-base-content swap-on fill-current h-6 w-6" />
+                </label>
+            </div>
+            <div className="flex-1">
+                <img src="logo.svg" className="ml-6" width={150} />
+            </div>
+        </div>
+
+    );
+};
+
+export default Header;
---
+++ frontend/src/components/Table.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {Column, flexRender, getCoreRowModel, useReactTable} from '@tanstack/react-table'
+
+type TableProps = {
+    data: any[],
+    columns: Array<any | Column<Record<string, unknown>>>;
+    renderSubComponent: (any) => void;
+}
+
+const Table: React.FC<TableProps> = ({data, columns, renderSubComponent}) => {
+    const [expanded, setExpanded] = React.useState({})
+
+    const table = useReactTable<any>({
+        data,
+        columns,
+        state: {
+            expanded,
+        },
+        onExpandedChange: setExpanded,
+        getSubRows: row => row.subRows,
+        getCoreRowModel: getCoreRowModel(),
+        debugTable: true,
+    })
+
+    return (
+        <table className="table">
+            <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+                <tr key={headerGroup.id}>
+                    {headerGroup.headers.map(header => (
+                        <th key={header.id}>
+                            {header.isPlaceholder
+                                ? null
+                                : flexRender(
+                                    header.column.columnDef.header,
+                                    header.getContext()
+                                )}
+                        </th>
+                    ))}
+                </tr>
+            ))}
+            </thead>
+            <tbody>
+            {table.getRowModel().rows.map(row => {
+                return (
+                    <>
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map(cell => {
+                                return (
+                                    <td key={cell.id}>
+                                        {flexRender(
+                                            cell.column.columnDef.cell,
+                                            cell.getContext()
+                                        )}
+                                    </td>
+                                )
+                            })}
+                        </tr>
+                        {row.getIsExpanded() && renderSubComponent && (
+                            <tr>
+                                {/* 2nd row is a custom 1 cell row */}
+                                <td colSpan={row.getVisibleCells().length}>
+                                    {renderSubComponent({row})}
+                                </td>
+                            </tr>
+                        )}
+                    </>
+                )
+            })}
+            </tbody>
+        </table>
+    );
+};
+
+export default Table;
