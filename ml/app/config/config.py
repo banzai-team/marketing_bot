86139b3327da6f0706c4eed32a0768a7705e7b24
Никита Савченко <nsavchenko@themsteam.com>
2023-11-10 13:21:52 +0700

    add classification skeleton

--- ml/app/config/config.py
+++ ml/app/config/config.py
@@ -1,1 +1,5 @@
 TOXISITY_MODEL_PATH = 'models/toxisity'
+EMBEDDER_MODEL_PATH = 'models/embedder'
+CLASSIFICATION_MODEL_PATH = 'models/classification/classification_model.pkl'
+
+FRAUD_THRESHOLD = 0.6
