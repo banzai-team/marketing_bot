d2ae921e02bacf20bddd09b1a2355b288bddf82d
wuz1mu <kingaal93@gmail.com>
2023-11-12 03:13:26 +0700

    log

--- backend/model-behavior/application/src/main/java/banz/ai/marketing/bot/modelbehavior/behavior/BehaviorHandler.java
+++ backend/model-behavior/application/src/main/java/banz/ai/marketing/bot/modelbehavior/behavior/BehaviorHandler.java
@@ -3,6 +3,8 @@ package banz.ai.marketing.bot.modelbehavior.behavior;
 import banz.ai.marketing.bot.commons.ModelBehaviorDTO;
 import banz.ai.marketing.bot.modelbehavior.behavior.exception.InvalidModelBehaviorException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,14 +13,17 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class BehaviorHandler {
 
+    private static Logger log = LoggerFactory.getLogger(BehaviorHandler.class);
     private final ModelBehaviorDomainRepository repository;
 
     @Transactional(readOnly = false)
     public void handleBehavior(ModelBehaviorDTO modelBehavior) throws InvalidModelBehaviorException {
+        log.info("Saving model behavior...");
         ModelBehaviorAggregate aggregate = ModelBehaviorAggregate.Builder
                 .forRequest(modelBehavior.getModelRequest())
                 .withResponse(modelBehavior.getModelResponse())
                 .build();
         repository.save(aggregate);
+        log.info("Aggregate was successfully saved");
     }
 }
