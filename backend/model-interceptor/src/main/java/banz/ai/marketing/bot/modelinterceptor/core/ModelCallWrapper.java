0f10d9f3dffd0de4946a6cfaded3915a83c7965b
wuz1mu <kingaal93@gmail.com>
2023-11-11 18:07:39 +0700

    add log

--- backend/model-interceptor/src/main/java/banz/ai/marketing/bot/modelinterceptor/core/ModelCallWrapper.java
+++ backend/model-interceptor/src/main/java/banz/ai/marketing/bot/modelinterceptor/core/ModelCallWrapper.java
@@ -4,6 +4,8 @@ import banz.ai.marketing.bot.commons.ModelBehaviorDTO;
 import banz.ai.marketing.bot.commons.ModelRequestDTO;
 import banz.ai.marketing.bot.commons.ModelResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +15,15 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class ModelCallWrapper {
 
+    private static Logger log = LoggerFactory.getLogger(ModelCallWrapper.class);
     private final ModelClient modelClient;
     private final RabbitTemplate rabbitTemplate;
 
     public ModelResponseDTO wrap(ModelRequestDTO modelRequest) {
+        log.info("Calling model... %s".formatted(modelRequest));
         var resp = modelClient.makeModelRequest(modelRequest);
         var modelResponse = resp.getBody();
+        log.info("Model responded::%s".formatted(modelResponse));
 
         var mqMessage = ModelBehaviorDTO.builder()
                 .capturedAt(new Date())
