7f529a49a4e3e82b3bfd9f8a8b4b8ed8abfc214b
Никита Савченко <nsavchenko@themsteam.com>
2023-11-12 00:33:42 +0700

    fix bug with small chat size

--- ml/app/core/classification/classification.py
+++ ml/app/core/classification/classification.py
@@ -30,8 +30,11 @@ class ClassificationModel:
                 embeddings = model_output.last_hidden_state[:, 0, :]
                 embeddings = torch.nn.functional.normalize(embeddings)
                 embeddings_list.append(embeddings)
-        sum_tensor = torch.stack(embeddings_list).sum(dim=0)
-        sum_tensor = torch.nn.functional.normalize(sum_tensor)
+        try:
+            sum_tensor = torch.stack(embeddings_list).sum(dim=0)
+            sum_tensor = torch.nn.functional.normalize(sum_tensor)
+        except:
+            sum_tensor = torch.tensor([[0]])
         return sum_tensor.cpu().numpy()
 
     def split_sentences(self, text):
@@ -43,7 +46,10 @@ class ClassificationModel:
 
     def predict(self, text):
         vector = self.embed_bert_cls(text)
-        return self.cl_model.predict_proba(vector)
+        if not vector.any():
+            return torch.tensor([[1,0,0]])
+        else:
+            return self.cl_model.predict_proba(vector)
 
 classification_model = ClassificationModel(embedder_path=EMBEDDER_MODEL_PATH, cl_path=CLASSIFICATION_MODEL_PATH)
 logging.info('Toxisity model loaded')
