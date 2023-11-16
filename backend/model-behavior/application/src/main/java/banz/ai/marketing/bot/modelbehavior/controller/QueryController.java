9da5dd834d5c426ee86ebe4a14fb44eb2e1582b5
Aleksey <deshevtsov@isands.ru>
2023-11-16 13:12:15 +0700

    order by performed at

--- backend/model-behavior/application/src/main/java/banz/ai/marketing/bot/modelbehavior/controller/QueryController.java
+++ backend/model-behavior/application/src/main/java/banz/ai/marketing/bot/modelbehavior/controller/QueryController.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,68 +27,64 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class QueryController {
 
-    private static Logger log = LoggerFactory.getLogger(QueryController.class);
-    private final ModelRequestQueryHandler modelRequestQueryHandler;
+  private static Logger log = LoggerFactory.getLogger(QueryController.class);
+  private final ModelRequestQueryHandler modelRequestQueryHandler;
 
-    @GetMapping("model-request")
-    public ResponseEntity<Page<ModelRequestListingItem>> listModelRequests(@RequestParam(value = "page", required = false) Integer page,
-                                                                           @RequestParam(value = "size", required = false) Integer size,
-                                                                           @RequestParam(value = "dialogId", required = false) Long dialogId
-    ) {
-        log.debug("Model reqyuest");
-        if (Objects.isNull(page)) {
-            page = 0;
-        }
-        if (Objects.isNull(size)) {
-            size = 100;
-        }
-        var query = ModelRequestListingQuery.builder().pageable(PageRequest.of(page, size));
-        if (Objects.nonNull(dialogId)) {
-            query.criteria(Map.of("dialogId", dialogId));
-        }
-        return ResponseEntity.ok().body(
-                modelRequestQueryHandler.list(query.build())
-                        .map(this::mapToDTO));
+  @GetMapping("model-request")
+  public ResponseEntity<Page<ModelRequestListingItem>> listModelRequests(@RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+                                                                         @RequestParam(value = "size", required = false, defaultValue = "10") Integer size,
+                                                                         @RequestParam(value = "sortBy", required = false, defaultValue = "performedAt") String sortBy,
+                                                                         @RequestParam(value = "sort", required = false, defaultValue = "asc") String sortDirection,
+                                                                         @RequestParam(value = "dialogId", required = false) Long dialogId
+  ) {
+    var query = ModelRequestListingQuery.builder()
+            .pageable(PageRequest.of(page, size, sortDirection.equals("desc") ? Sort.by(sortBy).descending() : Sort.by(sortBy).ascending()));
+    if (Objects.nonNull(dialogId)) {
+      query.criteria(Map.of("dialogId", dialogId));
     }
+    return ResponseEntity.ok().body(
+            modelRequestQueryHandler.list(query.build())
+                    .map(this::mapToDTO));
+  }
 
-    @GetMapping("model-request/{id}")
-    public ResponseEntity<ModelRequestListingItem> listModelRequests(@PathVariable("id") long id ) {
-        return ResponseEntity.ok()
-                .body(modelRequestQueryHandler.getById(ModelRequestByIdQueryDTO.builder().id(id).build())
-                .map(this::mapToDTO).orElseThrow());
+  @GetMapping("model-request/{id}")
+  public ResponseEntity<ModelRequestListingItem> listModelRequests(@PathVariable("id") long id) {
+    return ResponseEntity.ok()
+            .body(modelRequestQueryHandler.getById(ModelRequestByIdQueryDTO.builder().id(id).build())
+                    .map(this::mapToDTO).orElseThrow());
+  }
+
+  private ModelRequestListingItem mapToDTO(ModelRequest r) {
+    var reqBuilder = ModelRequestListItemDTO.builder()
+            .id(r.getId())
+            .dialogId(r.getDialog().getId())
+            .messages(r.getMessages().stream().map(ModelRequestMessage::getContent).collect(Collectors.toList()))
+            .text(r.getText())
+            .performedAt(r.getPerformedAt())
+            .isOperator(r.isOperator());
+    if (Objects.nonNull(r.getModelResponse())) {
+      reqBuilder.response(ModelResponseListItemDTO.builder()
+                      .id(r.getModelResponse().getId())
+                      .offerPurchase(r.getModelResponse().isOfferPurchase())
+                      .dialogEvaluation(r.getModelResponse().getDialogEvaluation())
+                      .stopTopics(r.getModelResponse().getStopTopics().stream().map(StopTopic::getContent).collect(Collectors.toList()))
+                      .feedback(r.getModelResponse().getFeedback())
+                      .build())
+              .build();
     }
+    var itemBuilder = ModelRequestListingItem.builder()
+            .request(reqBuilder.build());
 
-    private ModelRequestListingItem mapToDTO(ModelRequest r) {
-        var reqBuilder = ModelRequestListItemDTO.builder()
-                .id(r.getId())
-                .dialogId(r.getDialog().getId())
-                .messages(r.getMessages().stream().map(ModelRequestMessage::getContent).collect(Collectors.toList()))
-                .text(r.getText())
-                .performedAt(r.getPerformedAt())
-                .isOperator(r.isOperator());
-        if (Objects.nonNull(r.getModelResponse())) {
-            reqBuilder.response(ModelResponseListItemDTO.builder()
-                            .id(r.getModelResponse().getId())
-                            .offerPurchase(r.getModelResponse().isOfferPurchase())
-                            .dialogEvaluation(r.getModelResponse().getDialogEvaluation())
-                            .stopTopics(r.getModelResponse().getStopTopics().stream().map(StopTopic::getContent).collect(Collectors.toList()))
-                            .feedback(r.getModelResponse().getFeedback())
-                            .build())
-                    .build();
-        }
-        var itemBuilder = ModelRequestListingItem.builder()
-                .request(reqBuilder.build());
+    return itemBuilder.build();
+  }
 
-        return itemBuilder.build();
-    }
-
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ApiError> handleException(NotFoundException exception) {
-        return ResponseEntity.status(404).body(ApiError.builder()
-                .code(HttpStatus.NOT_FOUND.value())
-                .message("Not Found")
-                .timestamp(new Date())
-                .build()
-        );
-    }
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<ApiError> handleException(NotFoundException exception) {
+    return ResponseEntity.status(404).body(ApiError.builder()
+            .code(HttpStatus.NOT_FOUND.value())
+            .message("Not Found")
+            .timestamp(new Date())
+            .build()
+    );
+  }
 }
--- backend/model-behavior/application/src/test/resources/init-query-test.sql
+++ backend/model-behavior/application/src/test/resources/init-query-test.sql
@@ -2,9 +2,9 @@ INSERT INTO dialog (dialog_id, created_at)
 VALUES (1, CURRENT_TIMESTAMP),
        (2, CURRENT_TIMESTAMP);
 INSERT INTO model_request (model_request_id, dialog_id, is_operator, additional_text, performed_at)
-VALUES (1, 1, true, 'hello', CURRENT_TIMESTAMP),
-       (2, 1, true, 'jo', CURRENT_TIMESTAMP),
-       (3, 2, true, 'b', CURRENT_TIMESTAMP);
+VALUES (1, 1, true, 'hello', '2023-11-10 11:30:00 +00:00'),
+       (2, 1, true, 'jo', '2023-11-09 12:30:00 +00:00'),
+       (3, 2, true, 'b', '2023-11-11 12:00:00 +00:00');
 INSERT INTO model_response (model_response_id, dialog_evaluation, offer_purchase, model_request_id, feedback)
 VALUES (1, 0.5, true, 1, -4),
        (3, 0.2, true, 3, 2);
