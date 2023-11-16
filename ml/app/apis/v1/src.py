9f7bf8dbae47f8ca01753215e93628830bfdca58
Artem Astapenko <3767150+jamakase@users.noreply.github.com>
2023-11-16 16:53:55 +0300

    Sentiment model (#12)
    * change sentiment skeleton
    
    * coment old model src
    
    * Add gitignore
    
    * Add models v2
    
    ---------
    
    Co-authored-by: Никита Савченко <nsavchenko@themsteam.com>

--- ml/app/apis/v1/src.py
+++ ml/app/apis/v1/src.py
@@ -6,9 +6,12 @@ from app.config import FRAUD_THRESHOLD
 from app.core.toxisity_checker import toxisity_checker
 from app.core.stop_topics_checker import stop_topics_checker
 from app.core.classification import classification_model
+from app.core.sentiment_classification import sentiment_classification_model
 
 
 router = APIRouter(prefix="/v1")
+
+SENTIMENT_MAP = {'positive': 3, 'neutral': 1, 'negative': -4}
 
 
 @router.post('/base_process',
@@ -18,21 +21,29 @@ router = APIRouter(prefix="/v1")
              response_model=OutputBase)
 def process_base(input_: InputBase) -> OutputBase:
     stop_topics = set()
-    dialogue = '. '.join(input_.messages)
+    #dialogue = '. '.join(input_.messages)
     for message in input_.messages:
         stop_topics.update(toxisity_checker.classify(message))
         stop_topics.update(stop_topics_checker.classify(message))
-    classes = classification_model.predict(dialogue)[0]
-    if classes[2] >= FRAUD_THRESHOLD:
-        stop_topics.update('Найдены стоп темы, которые мы не можем точно определить')
-        sentimemt_loggit = -5.
+    #classes = classification_model.predict(dialogue)[0]
+    sentiment = sentiment_classification_model.predict(input_.messages[0])
+    sentiment_score = 0
+    for item in sentiment[0]:
+        sentiment_score += SENTIMENT_MAP[item['label']]*item['score']
+    if sentiment_score<=0:
         offer_confidence = False
     else:
-        class_ = classes[:2].argmax()
-        if class_ == 0:
-            offer_confidence=True
-            sentimemt_loggit = float(classes[:2][class_]*5.)
-        else:
-            offer_confidence=False
-            sentimemt_loggit = float(-classes[:2][class_]*5.)
-    return OutputBase(offer_confidence=offer_confidence, sentimemt_loggit=sentimemt_loggit, stop_topics=list(stop_topics))
+        offer_confidence = True
+    # if classes[2] >= FRAUD_THRESHOLD:
+    #     stop_topics.update('Найдены стоп темы, которые мы не можем точно определить')
+    #     sentimemt_loggit = -5.
+    #     offer_confidence = False
+    # else:
+    #     class_ = classes[:2].argmax()
+    #     if class_ == 0:
+    #         offer_confidence=True
+    #         sentimemt_loggit = float(classes[:2][class_]*5.)
+    #     else:
+    #         offer_confidence=False
+    #         sentimemt_loggit = float(-classes[:2][class_]*5.)
+    return OutputBase(offer_confidence=offer_confidence, sentimemt_loggit=sentiment_score, stop_topics=list(stop_topics))
